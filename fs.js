996b29310f2a7de1d73a65e672ed45bcc8b0f002
Eran Tiktin <etiktin@users.noreply.github.com>
2015-06-06 09:01:49 +0300

    Fix Stats binding
    This commit fixes issue isaacs/node-graceful-fs#35

--- fs.js
+++ fs.js
@@ -2,6 +2,7 @@
 // more evil than monkey-patching the native builtin?
 // Not sure.
 
+var fs = require('fs')
 var mod = require("module")
 var pre = '(function (exports, require, module, __filename, __dirname) { '
 var post = '});'
@@ -9,3 +10,10 @@ var src = pre + process.binding('natives').fs + post
 var vm = require('vm')
 var fn = vm.runInThisContext(src)
 fn(exports, require, module, __filename, __dirname)
+
+// fix reference to Stats constructor in node 0.11.13+
+var binding = process.binding('fs')
+if (binding.FSInitialize) {
+  binding.FSInitialize(fs.Stats)
+  module.exports.Stats = fs.Stats
+}
