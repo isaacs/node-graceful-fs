967262f6c43a7c94828f40dbd4ee58759f2bb4d9
isaacs <i@izs.me>
2015-06-25 17:44:50 -0700

    0.8 support: use 'instanceof' instead of '__proto__' test
    In Node 0.8 and 0.6, Object.create(null) still has a __proto__ member.

--- fs.js
+++ fs.js
@@ -8,7 +8,7 @@ function clone (obj) {
   if (obj === null || typeof obj !== 'object')
     return obj
 
-  if ('__proto__' in obj)
+  if (obj instanceof Object)
     var copy = { __proto__: obj.__proto__ }
   else
     var copy = Object.create(null)
