315d0c0160a95ca2d6e10177f3e047b1853f5d43
Christopher J. Brody <chris@brody.consulting>
2019-05-10 12:54:43 -0400

    Improve description on built-in stat ctor test

--- test/stats.js
+++ test/stats.js
@@ -5,7 +5,7 @@ var gfs = require('../graceful-fs.js')
 test('graceful fs uses same stats constructor as fs', function (t) {
   t.equal(gfs.Stats, fs.Stats, 'should reference the same constructor')
   t.ok(fs.statSync(__filename) instanceof fs.Stats,
-    'should be instance of fs.Stats')
+    'should be instance of fs.Stats (built-in fs.statSync call)')
   t.ok(gfs.statSync(__filename) instanceof fs.Stats,
     'should be instance of fs.Stats')
   t.end()
