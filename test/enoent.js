4c1b072b50391114dbb2be94a979e880388878ee
ZauberNerd <zaubernerd@zaubernerd.de>
2019-05-14 16:55:20 +0200

    Add basic test for stat functions with 3 arguments when supported
    This commit adds a simple test for the stat functions with three
    arguments to verify that they still work.
    The test is guarded by a version check to verify the current runtime
    supports optional options for the stat functions.

--- test/enoent.js
+++ test/enoent.js
@@ -3,6 +3,30 @@
 
 var t = require('tap')
 var g = require('../')
+
+var NODE_VERSION_MAJOR_WITH_BIGINT = 10
+var NODE_VERSION_MINOR_WITH_BIGINT = 5
+var NODE_VERSION_PATCH_WITH_BIGINT = 0
+var nodeVersion = process.versions.node.split('.')
+var nodeVersionMajor = Number.parseInt(nodeVersion[0], 10)
+var nodeVersionMinor = Number.parseInt(nodeVersion[1], 10)
+var nodeVersionPatch = Number.parseInt(nodeVersion[2], 10)
+
+function nodeSupportsBigInt () {
+  if (nodeVersionMajor > NODE_VERSION_MAJOR_WITH_BIGINT) {
+    return true
+  } else if (nodeVersionMajor === NODE_VERSION_MAJOR_WITH_BIGINT) {
+    if (nodeVersionMinor > NODE_VERSION_MINOR_WITH_BIGINT) {
+      return true
+    } else if (nodeVersionMinor === NODE_VERSION_MINOR_WITH_BIGINT) {
+      if (nodeVersionPatch >= NODE_VERSION_PATCH_WITH_BIGINT) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
 var file = 'this file does not exist even a little bit'
 var methods = [
   ['open', 'r'],
@@ -17,6 +41,13 @@ var methods = [
 if (process.version.match(/^v([6-9]|[1-9][0-9])\./)) {
   methods.push(['readdir', {}])
 }
+
+// any version > v10.5 can do stat(path, options, cb)
+if (nodeSupportsBigInt()) {
+  methods.push(['stat', {}])
+  methods.push(['lstat', {}])
+}
+
 
 t.plan(methods.length)
 methods.forEach(function (method) {
