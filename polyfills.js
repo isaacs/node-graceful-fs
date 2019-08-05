91ed07a9f1a5fd929c1e76288bccdffbfdf20468
Corey Farrell <git@cfware.com>
2019-08-05 19:21:58 -0400

    Remove v0.6.2 backport fix for fs.lchmod

--- polyfills.js
+++ polyfills.js
@@ -25,13 +25,6 @@ module.exports = patch
 function patch (fs) {
   // (re-)implement some things that are known busted or missing.
 
-  // lchmod, broken prior to 0.6.2
-  // back-port the fix here.
-  if (constants.hasOwnProperty('O_SYMLINK') &&
-      process.version.match(/^v0\.6\.[0-2]|^v0\.5\./)) {
-    patchLchmod(fs)
-  }
-
   // lutimes implementation, or no-op
   if (!fs.lutimes) {
     patchLutimes(fs)
@@ -150,49 +143,6 @@ function patch (fs) {
       }
     }
   }})(fs.readSync)
-
-  function patchLchmod (fs) {
-    fs.lchmod = function (path, mode, callback) {
-      fs.open( path
-             , constants.O_WRONLY | constants.O_SYMLINK
-             , mode
-             , function (err, fd) {
-        if (err) {
-          if (callback) callback(err)
-          return
-        }
-        // prefer to return the chmod error, if one occurs,
-        // but still try to close, and report closing errors if they occur.
-        fs.fchmod(fd, mode, function (err) {
-          fs.close(fd, function(err2) {
-            if (callback) callback(err || err2)
-          })
-        })
-      })
-    }
-
-    fs.lchmodSync = function (path, mode) {
-      var fd = fs.openSync(path, constants.O_WRONLY | constants.O_SYMLINK, mode)
-
-      // prefer to return the chmod error, if one occurs,
-      // but still try to close, and report closing errors if they occur.
-      var threw = true
-      var ret
-      try {
-        ret = fs.fchmodSync(fd, mode)
-        threw = false
-      } finally {
-        if (threw) {
-          try {
-            fs.closeSync(fd)
-          } catch (er) {}
-        } else {
-          fs.closeSync(fd)
-        }
-      }
-      return ret
-    }
-  }
 
   function patchLutimes (fs) {
     if (constants.hasOwnProperty("O_SYMLINK")) {
