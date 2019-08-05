20b77c49ccf44a2de97afb02c0cc80d1d5d7f126
Corey Farrell <git@cfware.com>
2019-08-05 16:48:10 -0400

    Create/use global shared retry queue.
    Importing a fresh copy of graceful-fs then using `require('fs').close`
    would only initiate retry of the queue from the first copy of
    graceful-fs, so needed retries associated with subsequent instances of
    graceful-fs would not be executed.
    
    PR-URL: https://github.com/isaacs/node-graceful-fs/pull/169
    Credit: @coreyfarrell
    Close: #169
    Reviewed-by: @isaacs

--- test/close.js
+++ test/close.js
@@ -1,10 +1,22 @@
-var fs$close = require('fs').close;
-var fs$closeSync = require('fs').closeSync;
-var fs = require('../');
+var fs = require('fs')
+var path = require('path')
+var gfsPath = path.resolve(__dirname, '..', 'graceful-fs.js')
+var gfs = require(gfsPath)
+var importFresh = require('import-fresh')
+var fs$close = fs.close
+var fs$closeSync = fs.closeSync
 var test = require('tap').test
 
 test('`close` is patched correctly', function(t) {
-  t.notEqual(fs.close, fs$close, 'patch close');
-  t.notEqual(fs.closeSync, fs$closeSync, 'patch closeSync');
+  t.match(fs$close.toString(), /graceful-fs shared queue/, 'patch fs.close');
+  t.match(fs$closeSync.toString(), /graceful-fs shared queue/, 'patch fs.closeSync');
+  t.match(gfs.close.toString(), /graceful-fs shared queue/, 'patch gfs.close');
+  t.match(gfs.closeSync.toString(), /graceful-fs shared queue/, 'patch gfs.closeSync');
+
+  var newGFS = importFresh(gfsPath)
+  t.equal(fs.close, fs$close)
+  t.equal(fs.closeSync, fs$closeSync)
+  t.equal(newGFS.close, fs$close)
+  t.equal(newGFS.closeSync, fs$closeSync)
   t.end();
 })
