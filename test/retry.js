569a726eca49685aeeb4e3325a32ae326186a0c7
nlf <quitlahok@gmail.com>
2021-07-20 13:40:00 -0700

    fix: start retrying immediately, stop after 60 seconds
    PR-URL: https://github.com/isaacs/node-graceful-fs/pull/217
    Credit: @nlf
    Close: #217
    Reviewed-by: @isaacs, @wraithgar

--- test/retry.js
+++ test/retry.js
@@ -7,17 +7,30 @@ var test = require('tap').test
 
 var EMFILE = Object.assign(new Error('FAKE EMFILE'), { code: 'EMFILE' })
 
-test('retries 10 times before erroring', function (t) {
-  var attempts = 0
-  realFs.readFile = function (path, options, cb) {
-    ++attempts
-    process.nextTick(cb, EMFILE)
-  }
-  var fs = importFresh(path.dirname(__dirname))
+test('eventually times out and returns error', function (t) {
+  var readFile = realFs.readFile
+  var realNow = Date.now
 
+  t.teardown(function () {
+    realFs.readFile = readFile
+    Date.now = realNow
+  })
+
+  realFs.readFile = function (path, options, cb) {
+    process.nextTick(function () {
+      cb(EMFILE)
+      // hijack Date.now _after_ we call the callback, the callback will
+      // call it when adding the job to the queue, we want to capture it
+      // any time after that first call so we can pretend it's been 60s
+      Date.now = function () {
+        return realNow() + 60000
+      }
+    })
+  }
+
+  var fs = importFresh(path.dirname(__dirname))
   fs.readFile('literally anything', function (err) {
     t.equal(err.code, 'EMFILE', 'eventually got the EMFILE')
-    t.equal(attempts, 10, 'tried 10 times')
     t.done()
   })
 })
