d834068941400b1457ad68ea4358ba5d80e509f4
isaacs <i@izs.me>
2022-04-01 08:18:44 -0700

    actually fix memory leak test failing spuriously
    There is no way to make the process heap tracking reliable in CI.  Just
    use v8 if available, otherwise skip the test.

--- test/avoid-memory-leak.js
+++ test/avoid-memory-leak.js
@@ -5,32 +5,31 @@ try {
   v8 = require('v8')
 } catch (er) {}
 
-var previousHeapStats
+if (!v8 || !v8.getHeapStatistics || typeof v8.getHeapStatistics().number_of_detached_contexts !== 'number') {
+  t.plan(0, 'no reliable context tracking available')
+  process.exit(0)
+}
+
+if (typeof global.gc !== 'function') {
+  t.plan(0, '--expose_gc not enabled')
+  process.exit(0)
+}
 
 function checkHeap (t) {
-  var v8stats = v8 ? v8.getHeapStatistics() : {}
-  var stats = process.memoryUsage()
-  if (typeof v8stats.number_of_detached_contexts === 'number')
-    t.equal(v8stats.number_of_detached_contexts, 0, 'no detached contexts')
-  else {
-    const memoryUsage = stats.heapUsed - previousHeapStats.heapUsed
-    const memoryUsageMB = Math.round(memoryUsage / Math.pow(1024, 2))
-    t.ok(memoryUsageMB < 10, 'expect less than 10MB difference, '
-      + memoryUsageMB + 'MB difference found.');
-  }
+  var v8stats = v8.getHeapStatistics()
+  t.equal(v8stats.number_of_detached_contexts, 0, 'no detached contexts')
 }
 
 t.test('no memory leak when loading multiple times', function(t) {
   t.plan(1);
   importFresh(process.cwd() + '/graceful-fs.js') // node 0.10-5 were getting: Cannot find module '../'
-  previousHeapStats = process.memoryUsage()
   // simulate project with 4000 tests
   var i = 0;
   function importFreshGracefulFs() {
     importFresh(process.cwd() + '/graceful-fs.js');
     if (i < 4000) {
       i++;
-      process.nextTick(() => importFreshGracefulFs());
+      process.nextTick(importFreshGracefulFs)
     } else {
       global.gc()
       checkHeap(t);
